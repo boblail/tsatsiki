6c100015aa58bd3d90a05497792255dc65bd35c8
Robert Lail <robert.lail@cph.org>
2011-03-11 11:59:10 -0600

    an empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsatsiki::Application.config.secret_token = '8a110611dc5c8ea3ff4192798da1f95ee0160e94486c83e2c2fa4366548b68cea403b45716966f3a5b386f7a1c004e325b36bd88cde6bb233a1d75f4549cd0b1'
