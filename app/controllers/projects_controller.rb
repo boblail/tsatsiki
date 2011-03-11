32ad4552ca5421d33f434c65deaa6027123026df
Robert Lail <robert.lail@cph.org>
2011-03-11 15:55:25 -0600

    refactored ProjectsController to use Responder pattern

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,83 +1,57 @@
 class ProjectsController < ApplicationController
-  # GET /projects
-  # GET /projects.xml
+  
+  
+  
   def index
     @projects = Project.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @projects }
-    end
+    respond_with(@projects)
   end
-
-  # GET /projects/1
-  # GET /projects/1.xml
+  
+  
+  
   def show
     @project = Project.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @project }
-    end
+    respond_with(@project)
   end
-
-  # GET /projects/new
-  # GET /projects/new.xml
+  
+  
+  
   def new
     @project = Project.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @project }
-    end
+    respond_with(@project)
   end
-
-  # GET /projects/1/edit
+  
+  
+  
   def edit
     @project = Project.find(params[:id])
+    respond_with(@project)
   end
-
-  # POST /projects
-  # POST /projects.xml
+  
+  
+  
   def create
     @project = Project.new(params[:project])
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to(@project, :notice => 'Project was successfully created.') }
-        format.xml  { render :xml => @project, :status => :created, :location => @project }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
-      end
-    end
+    @project.save
+    respond_with(@project)
   end
-
-  # PUT /projects/1
-  # PUT /projects/1.xml
+  
+  
+  
   def update
     @project = Project.find(params[:id])
-
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to(@project, :notice => 'Project was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
-      end
-    end
+    @project.update_attributes(params[:project])
+    respond_with(@project)
   end
-
-  # DELETE /projects/1
-  # DELETE /projects/1.xml
+  
+  
+  
   def destroy
     @project = Project.find(params[:id])
     @project.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(projects_url) }
-      format.xml  { head :ok }
-    end
+    respond_with(@project)
   end
+  
+  
+  
 end
