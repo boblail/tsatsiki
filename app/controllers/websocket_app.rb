e05d4713dbe862079323b8b39e62b84abdff6951
Bob Lail <bob.lailfamily@gmail.com>
2011-03-12 11:00:19 -0600

    fleshed out WebSocket communications for running tests

--- app/controllers/websocket_app.rb
+++ app/controllers/websocket_app.rb
@@ -2,9 +2,13 @@ require 'rack/websocket'
 
 class WebsocketApp < Rack::WebSocket::Application
   
+  ACCEPTED_MESSAGES = %w{hello execute}
+  
+  
   def self.call(env)
     self.new.call(env)
   end
+  
   
   def on_open
     Rails.logger.debug "Client connected"
@@ -14,17 +18,46 @@ class WebsocketApp < Rack::WebSocket::Application
     Rails.logger.debug "Client disconnected"
   end
   
-  def on_message(message)
-    case message
-    when /hello/
-      send_data({:key => "value"})
-    when /cucumber/
-      send_data({:key => "value"})
-    end
-  end
-  
   def on_error(error)
     Rails.logger.error "Error occured: " + error.message
   end
+  
+  def on_message(json)
+    json = JSON.parse(json)
+    message = json['message']
+    data = json['data']
+    if valid_message?(message)
+      send("on_#{message}", data)
+    else
+      Rails.logger.error "Unrecognized message \"#{message}\""
+    end
+  end
+  
+  
+  def send_message(message, data=nil)
+    send_data({
+      :message => message,
+      :data => data
+    }.to_json)
+  end
+  
+  
+private
+  
+  
+  def valid_message?(message)
+    ACCEPTED_MESSAGES.member?(message)
+  end
+  
+  
+  def on_hello
+    send_message('hi')
+  end
+  
+  
+  def on_execute(params)
+    send_message('ok')
+  end
+  
   
 end
