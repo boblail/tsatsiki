650da707cfedb650989b060f847654e78704e198
Robert Lail <robert.lail@cph.org>
2011-06-29 16:09:44 -0500

    allow traversing scenarios with the up, down keys

--- app/assets/javascripts/jquery_ext.js
+++ app/assets/javascripts/jquery_ext.js
@@ -1,5 +1,6 @@
 /* Extend jQuery with functions for PUT and DELETE requests. */
 jQuery.extend({
+  
   put: function(url, data, callback, type) {
     if(jQuery.isFunction(data)) {
       callback = data;
@@ -10,6 +11,7 @@ jQuery.extend({
     data['_method'] = 'put';
     return jQuery.post(url, data, callback, type);
   },
+  
   destroy: function(url, data, callback, type) {
     if(jQuery.isFunction(data)) {
       callback = data;
@@ -20,4 +22,41 @@ jQuery.extend({
     data._method = 'delete';
     return jQuery.post(url, data, callback, type);
   }
+  
+});
+
+jQuery.fn.extend({
+  
+  up: function(selector) {
+    var previous = this.prev(selector),
+        parent = this.parent(),
+        previous_parent;
+    
+    // Look up the tree until we run out of parents
+    while((previous.length == 0) && (parent.length > 0)) {
+      
+      // Does the parent's younger sibling have any descendents that match the selector?
+      previous = parent.prev().find(selector).last();
+      parent = parent.parent();
+    }
+    
+    return previous;
+  },
+  
+  down: function(selector) {
+    var next = this.next(selector),
+        parent = this.parent(),
+        next_parent;
+    
+    // Look down the tree until we run out of parents
+    while((next.length == 0) && (parent.length > 0)) {
+      
+      // Does the parent's older sibling have any descendents that match the selector?
+      next = parent.next().find(selector).first();
+      parent = parent.parent();
+    }
+    
+    return next;
+  }
+  
 });
