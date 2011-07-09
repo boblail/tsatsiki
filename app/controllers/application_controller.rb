3d8bf372c6d1dadfd861783e83843f70ac4a33f1
Bob Lail <bob.lailfamily@gmail.com>
2011-07-09 14:18:17 -0500

    styled devise pages for signing up, signing in, and resetting password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,32 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   respond_to :html
   
+  layout :layout_by_resource
+  
+  
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
   
+  
 protected
+  
   
   def pjax?
     params[:_pjax] == "true"
   end
+  
+  
+private
+  
+  
+  def layout_by_resource
+    if devise_controller?
+      "authentication"
+    else
+      "application"
+    end
+  end
+  
   
 end
