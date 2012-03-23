714ac1d1bd0f73a89115e1686324306df0a91051
Robert Lail <robert.lail@cph.org>
2012-03-23 13:55:30 -0500

    fix for list of features: only apply specified ID to root element (not to all nested trees)

--- app/helpers/feature_helper.rb
+++ app/helpers/feature_helper.rb
@@ -5,7 +5,8 @@ module FeatureHelper
   def render_features(features, options={})
     return "" if features.empty?
     
-    ul_options = options.pick(:id).merge(:class => "tree features")
+    ul_options = {:class => "tree features"}
+    ul_options.merge!(:id => options.delete(:id)) if options.key?(:id)
     content_tag(:ul, ul_options) do
       render(:partial => "projects/feature", :collection => features, :locals => {:options => options})
     end
